db7e9925c8298a9ee0142a1718208c0d2973d4af
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-03-14 19:59:36 -0300

    Add files via upload

---
+++ primeirosScripts09032025/ex001.py
@@ -0,0 +1,16 @@
+nome = "Vitor"
+sobrenome = "Natal Cordeiro"
+idade = 17
+datanasc = 2007
+maiordeidade = idade >= 18
+alt = 1.86
+
+
+
+
+print(f"Nome: {nome}")
+print(f"Sobrenome: {sobrenome}")
+print(f"Idade: {idade}")
+print(f"Data de nascimento: {datanasc}")
+print(f"É maior de idade? {maiordeidade}")
+print(f"Altura em metros {alt}")
