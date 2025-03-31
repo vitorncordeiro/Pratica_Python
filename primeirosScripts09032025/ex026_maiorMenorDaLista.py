e19014e04e7ccfed5e91f68dd497d4cd1eea9283
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-03-30 21:12:22 -0300

    Update ex026_maiorMenorDaLista.py

--- primeirosScripts09032025/ex026_maiorMenorDaLista.py
+++ primeirosScripts09032025/ex026_maiorMenorDaLista.py
@@ -1,13 +1,13 @@
-listaValores = []
+setValores = set()
 while True:
     perguntaInicial = input('Deseja inserir um valor? [S] [N]\n').upper()
     if perguntaInicial.startswith('S'):
         valor = int(input('Digite um valor: '))
-        listaValores.append(valor)
+        setValores.add(valor)
     elif perguntaInicial.startswith('N'):
         break
     else:
         print('Selecione uma opção válida!')
         continue
-        
-print(f'MAIOR: {max(listaValores)}\nMENOR: {min(listaValores)}')
+totalCrescente = sorted(list(setValores))
+print(f'MAIOR: {max(setValores)}\nMENOR: {min(setValores)}\nTOTAL: {totalCrescente}')
