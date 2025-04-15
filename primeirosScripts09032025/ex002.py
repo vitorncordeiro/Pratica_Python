3fad58941c2d6bdf7cb26bfbd6860de1c88308c3
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-04-15 16:24:02 -0300

    Atualizar o ex002.py

--- primeirosScripts09032025/ex002.py
+++ primeirosScripts09032025/ex002.py
@@ -1,6 +1,6 @@
 #imc = peso/altura x altura
 nome = "Vitor"
-peso = 80.5
+peso = 81.5
 altura = 1.86
 imc = peso / (altura ** 2)
 print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}')
