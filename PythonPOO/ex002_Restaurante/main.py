bdd2087f273f357dda1167b7a323d5572de4c88b
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-04-23 15:25:20 -0300

    Update main.py

--- PythonPOO/ex002_Restaurante/main.py
+++ PythonPOO/ex002_Restaurante/main.py
@@ -3,6 +3,7 @@ class Restaurant:
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
         self.open = open
+        self.clientesAtuais = 0
     def open_restaurant(self): 
         if self.open:
             print('Já está aberto')
@@ -15,6 +16,19 @@ class Restaurant:
         else:
             status = 'Fechado'
         return print(f'Nome do restaurante: {self.restaurant_name}\nTipo do restaurante: {self.cuisine_type}\nStatus: {status}')
+    
+    def show_number_served(self):
+        print(f'Número de clientes sendo atendidos em {self.restaurant_name}: {self.clientesAtuais}')
+    def increment_number_served(self, numberAcc):
+        self.clientesAtuais = self.clientesAtuais + numberAcc
+        print(f'Número de clientes sendo atendidos em {self.restaurant_name}: {self.clientesAtuais}')
+    def decrement_number_served(self, numberDec):
+        if numberDec <= self.clientesAtuais:
+            self.clientesAtuais = self.clientesAtuais - numberDec
+            self.show_number_served()
+        else:
+            raise Exception
+    
 
 Madero = Restaurant('Madero', 'Hamburgueria')
 Kikuti = Restaurant('Kikuti', 'Japonês')
@@ -24,4 +38,11 @@ listaRestaurantes = [Madero, Kikuti, OneHundredBeer]
 for restaurante in listaRestaurantes:
     restaurante.open_restaurant()
     restaurante.describe_restaurante()
-    print()
+    print() 
+
+
+Madero.increment_number_served(100)
+print('###########################')
+Madero.increment_number_served(400)
+print('###########################')
+Madero.decrement_number_served(50)
