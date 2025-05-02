13a4c10d75fd23aa40ae4b3699dfe7514246158a
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-02 16:19:07 -0300

    Add files via upload

---
+++ PythonPOO/ex008_eletrodomestico/main.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+class Eletrodomestico:
+    def __init__(self, nome, ligado=False):
+        self._nome = nome
+        self.ligado = ligado
+    @property
+    def nome(self):
+        return self._nome
+    @abstractmethod
+    def ligar(self):
+        self.ligado = True
+class InterfaceUSB:
+    def __init__(self, conectarUSB=False):
+        self.conectarUSB = conectarUSB
+    @abstractmethod
+    def conectar(self):
+        self.conectarUSB = True
+class SmartSpeaker(Eletrodomestico, InterfaceUSB):
+    def __init__(self, nome):
+        super().__init__(nome)
+    def ligar(self):
+        self.ligado = True
+        print('Objeto ligado')
+    def conectar(self):
+        self.conectarUSB = True
+        print('USB conectado')
+Alexa = SmartSpeaker('Alexa')
+print(Alexa.nome)
+Alexa.ligar()
+Alexa.conectar()
