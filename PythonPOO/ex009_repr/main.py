c40347229491d0c6c342e18d13f49ca952530f94
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-04 14:20:59 -0300

    Add files via upload

---
+++ PythonPOO/ex009_repr/main.py
@@ -0,0 +1,36 @@
+# Funções decoradoras e decoradores com classes
+class myRepr:
+    def __repr__(self):
+        class_name = self.__class__.__name__                                                                           
+        class_dict = self.__dict__
+        class_repr = f'{class_name}({class_dict})'
+        return class_repr
+
+class Time:
+     
+    def __init__(self, nome):
+        self.nome = nome
+
+    __repr__ = myRepr.__repr__
+    
+ 
+
+
+class Planeta:
+    def __init__(self, nome):
+        self.nome = nome
+        self._repr = myRepr()
+    def __repr__(self):
+        self._repr.nome = self.nome
+        return self._repr.__repr__()
+brasil = Time('Brasil')
+portugal = Time('Portugal')
+
+terra = Planeta('Terra')
+marte = Planeta('Marte')
+
+print(brasil)
+print(portugal)
+
+print(terra)
+print(marte)
