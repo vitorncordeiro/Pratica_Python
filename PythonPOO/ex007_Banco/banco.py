3dce287d85a4c8de963d05900e1221a9af5ca243
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-05 17:01:39 -0300

    Update banco.py

--- PythonPOO/ex007_Banco/banco.py
+++ PythonPOO/ex007_Banco/banco.py
@@ -1,1 +1,11 @@
 from contas import ContaCorrente, ContaPoupanca
+class Banco:
+    def __init__(self, nome):
+        self.nome = nome
+        self.AGENCIA = 4321
+        self.conta = None
+        self.cliente = None
+    def sacar():
+        pass
+
+        
