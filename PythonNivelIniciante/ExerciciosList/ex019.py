deb95b5c62fc1228e27805b4836edc73670cbcd4
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-12 17:18:48 -0300

    Update ex019.py

--- PythonNivelIniciante/ExerciciosList/ex019.py
+++ PythonNivelIniciante/ExerciciosList/ex019.py
@@ -1,1 +1,45 @@
+sistemasEVotos = [["Windows Server", 0], ["Unix", 0], ["Linux", 0], ["Netware", 0],\
+                     ["Mac OS", 0], ["Outro", 0]]
+contadorDeVotos = 0
+while True:
+    
+    numeroEscolhido = input('"Qual o melhor Sistema Operacional para uso em servidores?"\n\
+    As possíveis respostas são:\n\
+    1- Windows Server\n\
+    2- Unix\n\
+    3- Linux\n\
+    4- Netware\n\
+    5- Mac OS\n\
+    6- Outro\n')
+    if numeroEscolhido.isnumeric() == False:
+        print('Informe um número ')
+    else:
+        numeroEscolhido = int(numeroEscolhido)
+        if numeroEscolhido > 6 or numeroEscolhido < 0:
+            print("Informe um valor entre 1 e 6 ou 0 para sair!")
+    if numeroEscolhido == 0:
+        print(f'\nResultado da votação:')
+        break
+
+    sistemasEVotos[numeroEscolhido - 1][1] += 1 #somando 1 ponto pro sistema votado
+    contadorDeVotos += 1
+
+    
+sistemasVotados = []
+for parValores in sistemasEVotos:
+    if parValores[1] > 0:
+        sistemasVotados.append(parValores)
+sistemasVotados.sort(reverse=True, key=lambda a: a[1])
+msgFormatada = f"Foram computados {contadorDeVotos} votos.\n\nJogador:        Votos:        %"
+
+def calcularPercentual(nvotos, TotalVotos):
+    resultado = nvotos/TotalVotos*100
+    return resultado
+for jogador in sistemasVotados:
+    msgFormatada = msgFormatada + f"\n{jogador[0]}        {jogador[1]}        {calcularPercentual(jogador[1], contadorDeVotos):.2f}"
+jogadorMaisVotado = max(sistemasVotados, key=lambda a: a[1])    
+msgFinal = f"{msgFormatada}\nO melhor jogador foi o {jogadorMaisVotado[0]}, com "\
+        f"{jogadorMaisVotado[1]} votos, correspondendo a {calcularPercentual(jogadorMaisVotado[1], contadorDeVotos):.2f}% do total de votos"
+print(msgFinal)
+
 
