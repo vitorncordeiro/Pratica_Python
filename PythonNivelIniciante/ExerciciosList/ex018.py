f739aeacb4f80581e1e020213c250e7011004244
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-12 17:18:19 -0300

    Update ex018.py

--- PythonNivelIniciante/ExerciciosList/ex018.py
+++ PythonNivelIniciante/ExerciciosList/ex018.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 caminho = Path(__file__).parent / 'save.txt'
 
-jogadores_e_votos = []
+jogadoresEVotos = []
 contadorDeVotos = 0
 while True:
     
@@ -14,28 +14,28 @@ while True:
             print("Informe um valor entre 1 e 23 ou 0 para sair!")
 
     for i in range(1, 24): #criando lista com 23 jogadores
-        jogadores_e_votos.append([f'Jogador nº {i}', 0])
+        jogadoresEVotos.append([f'Jogador nº {i}', 0])
     if numeroEscolhido == 0:
         
         print(f'\nResultado da votação:')
         break
-    jogadores_e_votos[numeroEscolhido - 1][1] += 1 #somando 1 ponto pro jogador votado
+    jogadoresEVotos[numeroEscolhido - 1][1] += 1 #somando 1 ponto pro jogador votado
     contadorDeVotos += 1
 
     
-jogadores_votados = []
-for parValores in jogadores_e_votos:
+jogadoresVotados = []
+for parValores in jogadoresEVotos:
     if parValores[1] > 0:
-        jogadores_votados.append(parValores)
-jogadores_votados.sort(reverse=True, key=lambda a: a[1])
+        jogadoresVotados.append(parValores)
+jogadoresVotados.sort(reverse=True, key=lambda a: a[1])
 msgFormatada = f"Foram computados {contadorDeVotos} votos.\n\nJogador:        Votos:        %"
 
 def calcularPercentual(nvotos, TotalVotos):
     resultado = nvotos/TotalVotos*100
     return resultado
-for jogador in jogadores_votados:
+for jogador in jogadoresVotados:
     msgFormatada = msgFormatada + f"\n{jogador[0]}        {jogador[1]}        {calcularPercentual(jogador[1], contadorDeVotos):.2f}"
-jogadorMaisVotado = max(jogadores_votados, key=lambda a: a[1])    
+jogadorMaisVotado = max(jogadoresVotados, key=lambda a: a[1])    
 msgFinal = f"{msgFormatada}\nO melhor jogador foi o {jogadorMaisVotado[0]}, com "\
         f"{jogadorMaisVotado[1]} votos, correspondendo a {calcularPercentual(jogadorMaisVotado[1], contadorDeVotos):.2f}% do total de votos"
 print(msgFinal)
