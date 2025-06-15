9454b5945b4dc053813065df3d20f533966d2d1c
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-06-14 22:01:23 -0300

    Update aSoma.py

--- PythonNivelIniciante/aSoma.py
+++ PythonNivelIniciante/aSoma.py
@@ -4,7 +4,7 @@ for x in range(quantidadeDeNumerosPSomar):
     if x == 0:
         soma = int(input('Digite o primeiro número: '))
     elif x == quantidadeDeNumerosPSomar - 1:
-        soma = int(input('Digite o último número: '))
+        soma = int(input('Digite o último número:  '))
     else:
         soma = int(input("Digite o próximo número: "))        
 
